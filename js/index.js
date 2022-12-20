1d4574fe801e2362e606db0b0f6115e5e6566354
Danielle <danielle@kendallspace.net>
2022-12-20 17:47:31 -0500

    add animated text to index.html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 // start game modal initializations
 const fightBtn = document.getElementById("fightBtn");
 const startGameModal = document.getElementById("startGameModal");
-const modalOverlay = document.getElementById("modalOverlay");
 
-//pop open the start game modal with overlay
-const openStartGameModal = () => {
-    startGameModal.classList.add("active");
+// to show fight button
+const showHidden = () => {
+    fightBtn.classList.remove("hidden");
 }
-openStartGameModal();
+
+// add intro message one character at a time, to simulate typing
+const typeText = (div, message, len) => {
+    if (len > message.length) return;
+    div.innerText = message.substr(0, len);
+    setTimeout(function() {typeText(div, message, len + 1); }, 65);
+}
+
+// start screen and writer
+let div = document.createElement("div");
+let welcomeContainer = document.getElementById("welcomeMessage");
+welcomeContainer.appendChild(div);
+typeText(div, "Welcome, Captain.\n\nEarth is being attacked by a horde of aliens.\n\nWill you help save the Earth?", 0);
+
+// after intro message is done displaying, show fight button
+showHidden();
 
 // commence battle!
 fightBtn.addEventListener("click", () => {
