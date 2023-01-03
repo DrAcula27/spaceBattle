8a5d6727614a4b0993617fd95d01d0cc0e32c9fd
Danielle <danielle@kendallspace.net>
2023-01-03 18:16:01 -0500

    add recharge button; change functionality so that player attacks one ship at a time

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 // create a function to get a random integer between 2 values, inclusive
 const getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 // set global variables
 const NUM_ALIEN_SHIPS = getRandomIntInclusive(3, 10);
@@ -13,7 +13,7 @@ const playerShip = document.getElementById("player-ship");
 const playerShipHull = document.getElementById("player-ship-hull");
 const aliens = document.getElementById("aliens");
 const alienShipHull = document.getElementsByClassName("alien-ship-hull");
-const startGameBtn = document.getElementById("startGameBtn");
+const attackBtn = document.getElementById("attackBtn");
 const backgroundMusic = document.getElementById("spaceSound");
 
 // set volume to 10%
@@ -21,19 +21,19 @@ backgroundMusic.volume = 0.1;
 
 // create a Ship class for making the player's ship
 class Ship {
-    constructor(hull, firepower, accuracy) {
-        this.hull = hull;
-        this.firepower = firepower;
-        this.accuracy = accuracy;
+  constructor(hull, firepower, accuracy) {
+    this.hull = hull;
+    this.firepower = firepower;
+    this.accuracy = accuracy;
+  }
+  attack(enemy) {
+    if (Math.random() < this.accuracy) {
+      enemy.hull -= this.firepower;
     }
-    attack(ship) {
-        if (Math.random() < this.accuracy) {
-            ship.hull -= this.firepower;
-        }
-    }
-    retreat() {
-        window.location.reload();
-    }
+  }
+  retreat() {
+    window.location.reload();
+  }
 }
 
 // create the player's ship: USS HelloWorld, with the required specs
@@ -42,102 +42,104 @@ USS_HelloWorld.attacking = true;
 
 // create a factory to make and store alien ships
 class AlienFleet {
-    constructor() {
-        this.alienShips = [];
-    }
-    addAlienShip() {
-        // alien hull needs to be between 3 and 6, inclusive
-        let hull = getRandomIntInclusive(3, 6);
-        // alien firepower needs to be between 2 and 4, inclusive
-        let firepower = getRandomIntInclusive(2, 4);
-        // alien accuracy needs to be between 0.6 and 0.8, inclusive
-        let accuracy = Math.random() * (0.81 - 0.6) + 0.6;
-        accuracy = accuracy.toFixed(2);
-        // create the alien ship
-        let newAlienShip = new Ship(hull, firepower, +accuracy);
-        // add the new ship to the array of ships
-        this.alienShips.push(newAlienShip);
-    }
+  constructor() {
+    this.alienShips = [];
+  }
+  addAlienShip() {
+    // alien hull needs to be between 3 and 6, inclusive
+    let hull = getRandomIntInclusive(3, 6);
+    // alien firepower needs to be between 2 and 4, inclusive
+    let firepower = getRandomIntInclusive(2, 4);
+    // alien accuracy needs to be between 0.6 and 0.8, inclusive
+    let accuracy = Math.random() * (0.81 - 0.6) + 0.6;
+    accuracy = accuracy.toFixed(2);
+    // create the alien ship
+    let newAlienShip = new Ship(hull, firepower, +accuracy);
+    // add the new ship to the array of ships
+    this.alienShips.push(newAlienShip);
+  }
 }
 
 // generate the array of alien ships
 let alienFleet = new AlienFleet();
 for (let i = 1; i <= NUM_ALIEN_SHIPS; i++) {
-    alienFleet.addAlienShip();
-    aliens.innerHTML += `
-        <div id="alien${i}" class="alien-ship">
-            <img id="alienImg${i}" src="../images/enemy_ship.png" alt="alien ship">
+  alienFleet.addAlienShip();
+  aliens.innerHTML += `
+        <div id="alien" class="alien-ship">
+            <img id="alienImg" src="../images/enemy_ship.png" alt="alien ship">
             <h3 class="alien-ship-title">Alien</h3>
             <h4>Hull: <span class="alien-ship-hull">0</span></h4>
         </div>
     `;
 }
 
-// update alien ship hull
-const alienHullUpdate = () => {
-    for (let i = 0; i < NUM_ALIEN_SHIPS; i++) {
-        alienShipHull[i].innerHTML = alienFleet.alienShips[i].hull;
-    }
-}
-alienHullUpdate();
+// gives access to the array of alien ships
+const aliensArray = alienFleet.alienShips;
+console.log(aliensArray.length);
 
-// have player fight each alien one at a time
-const spaceBattle = () => {
-    let aliens = alienFleet.alienShips;
-    // loop through the array of alien ships
-    for (let i = 0; i < aliens.length; i++) {
-        // check if player's ship is destroyed (hull damage reaches 0)
-        if (USS_HelloWorld.hull <= 0) {
-            USS_HelloWorld.hull = 0;
-            USS_HelloWorld.attacking = false;
-            const playerDeadImg = document.getElementById("playerShipImg");
-            playerDeadImg.src = "../images/enemy_ship_dead.png";
-            openEndGameModal();
-            displayEndGameMessage();
-            restartGame();
-            break;
-        }
-        // player and alien fight it out until one of them is destroyed
-        let notDestroyed = true;
-        while (notDestroyed) {
-            // player has advantage, and gets to go first
-            USS_HelloWorld.attack(aliens[i]);
-            // update alien hull in HTML page
-            alienHullUpdate();
-            // check if alien is destroyed
-            if (aliens[i].hull <= 0) {
-                // set hull to 0; you can't have a negative health!
-                aliens[i].hull = 0;
-                // mark the ship as destroyed
-                const alienImage = document.getElementById(`alienImg${i+1}`);
-                alienImage.src  = "../images/enemy_ship_dead.png";
-                // end attack cycle
-                break;
-            }
-            // alien ship attacks player
-            aliens[i].attack(USS_HelloWorld);
-            // update USS_HelloWorld hull in HTML page
-            playerShipHull.innerHTML = USS_HelloWorld.hull;
-            // check if player ship destroyed
-            if (USS_HelloWorld.hull <= 0) {
-                // set hull to 0; you can't have a negative health!
-                USS_HelloWorld.hull = 0;
-                // end attack cycle
-                break;
-            }
-        }
-        checkWinner();
-    }
+// update alien fleet hull values
+for (let i = 0; i < NUM_ALIEN_SHIPS; i++) {
+    alienShipHull[i].innerHTML = alienFleet.alienShips[i].hull;
 }
 
-//check for a winner: wether player destroyed
+// have player fight one alien ship in the fleet
+const fightOneAlien = () => {
+  // player and alien fight it out until one of them is destroyed
+  let playerDestroyed = false;
+  while (!playerDestroyed) {
+    // player has advantage, and gets to go first
+    USS_HelloWorld.attack(aliensArray[0]);
+    // update alien hull in HTML page
+    alienShipHull[0].innerHTML = aliensArray[0].hull;
+    // check if alien is destroyed
+    if (aliensArray[0].hull <= 0) {
+      // set hull to 0; you can't have a negative health!
+      aliensArray[0].hull = 0;
+      alienShipHull[0].innerHTML = 0;
+      // mark the ship as destroyed
+      const alienImage = document.getElementById(`alienImg`);
+      alienImage.src = "../images/explosion.gif";
+      // after a timeout, remove the alien from the fleet
+      setTimeout(() => {
+        aliensArray.shift();
+        const deadAlien = document.getElementById("alien");
+        deadAlien.parentNode.removeChild(deadAlien);
+      }, 1260);
+      // end attack cycle
+      break;
+    }
+    // alien ship attacks player
+    aliensArray[0].attack(USS_HelloWorld);
+    // update USS_HelloWorld hull in HTML page
+    playerShipHull.innerHTML = USS_HelloWorld.hull;
+    // check if player ship destroyed
+    if (USS_HelloWorld.hull <= 0) {
+      // set hull to 0; you can't have a negative health!
+      USS_HelloWorld.hull = 0;
+      playerShipHull.innerHTML = 0;
+      // mark the ship as destroyed
+      const playerShipImage = document.getElementById(`playerShipImg`);
+      playerShipImage.src = "../images/explosion.gif";
+      // after a timeout, remove the player ship
+      setTimeout(() => {
+        playerShip.parentNode.removeChild(playerShip);
+      }, 1260);
+      // end while loop
+      playerDestroyed = true;
+      // end attack cycle
+      break;
+    }
+  }
+};
+
+//check for a winner
 const checkWinner = () => {
-    if (USS_HelloWorld.hull > 0) {
-        openEndGameModal();
-        displayEndGameMessage();
-        restartGame();
-    }
-}
+  if (USS_HelloWorld.hull <= 0 || aliensArray.length === 0) {
+    openEndGameModal();
+    displayEndGameMessage();
+    restartGame();
+  }
+};
 
 //end of game modal initializations
 const endGameModal = document.getElementById("endGameModal");
@@ -146,26 +148,32 @@ const restartButton = document.getElementById("restartButton");
 
 //pop open the end game modal with overlay
 const openEndGameModal = () => {
-    endGameModal.classList.add("active");
-    modalOverlay.classList.add("active");
-}
+  endGameModal.classList.add("active");
+  modalOverlay.classList.add("active");
+};
 
 //populate modal title
 const displayEndGameMessage = () => {
-    return USS_HelloWorld.hull > 0
+  return USS_HelloWorld.hull > 0
     ? (endGameModalTitle.textContent = `You've defeated all ${NUM_ALIEN_SHIPS} aliens!\n And you still have ${USS_HelloWorld.hull} hull left!`)
     : (endGameModalTitle.textContent = `Your ship was destroyed! GAME OVER`);
-}
+};
 
 //restart the game
 const restartGame = () => {
-    restartButton.addEventListener("click", () => {
-        window.location.reload();
-    });
-}
+  restartButton.addEventListener("click", () => {
+    window.location.reload();
+  });
+};
 
-// commence battle!
-startGameBtn.addEventListener("click", () => {
-    // loop until player destroyed or all alien ships destroyed
-    spaceBattle();
+// if player clicks the attack! button:
+attackBtn.addEventListener("click", () => {
+    // fight one ship until either player or alien destroyed
+    fightOneAlien();
+    // check for a winner; timeout is necessary due to the timeout used earlier to remove alien ships from the array
+    setTimeout(() => {
+        checkWinner();
+    }, 1500);
 });
+
+// if player clicks the recharge button:
